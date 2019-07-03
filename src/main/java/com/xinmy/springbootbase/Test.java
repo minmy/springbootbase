75c1fb5c37c21b66b5207bdd4e090e1a3dcee7cf
minmy <546019300@qq.com>
2019-07-03 17:42:58 +0800

    dev test

---
+++ src/main/java/com/xinmy/springbootbase/Test.java
@@ -0,0 +1,20 @@
+package com.xinmy.springbootbase;
+
+/**
+ * @author lijianxin
+ * @date 2019/7/3 15:19
+ * @desc
+ */
+public class Test {
+    /**
+     * @param p1
+     * @param p2
+     * @return java.lang.String
+     * @author lijianxin
+     * @date 2019/7/3 16:05
+     * @desc
+     */
+    public String test(int p1,int p2){
+        return "";
+    }
+}
